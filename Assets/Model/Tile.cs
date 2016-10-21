8a56e1f39d5add059b904a523edbeba8324765fd
Chris Baiamonte <baiamonte.chris@gmail.com>
2016-10-21 18:21:51 -0400

    Initial commit

---
+++ Assets/Model/Tile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+public enum TileType { Empty, Floor }
+
+public class Tile
+{
+
+    TileType _tileType = TileType.Empty;
+
+    World _world;
+    int _x;
+    int _y;
+
+    LooseObject _looseObject;
+    InstalledObject _installedObject;
+
+    public Tile(World world, int x, int y)
+    {
+        _world = world;
+        _x = x;
+        _y = y;
+
+    }
+}
---
+++ Assets/Model/World.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+
+public class World {
+
+    Tile[,] _tiles;
+    int _width;
+    int _height;
+
+    public World(int width = 100, int height = 100)
+    {
+        _width = width;
+        _height = height;
+
+        _tiles = new Tile[_width, _height];
+    }
+
+}
